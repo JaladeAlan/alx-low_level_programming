dc38a23c6b88de70f48ec84be02ce1b430527de2
Alalade Ayodeji <ayodejialalade29@gmail.com>
2023-06-16 16:22:00 -0700

    program that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,14 @@
 int main(void)
 {
     /*declare a variable*/
-  char alphabet;
+  char alphabet;  
   
- for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+  for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
     {
       /* print the value of alphabet */
       putchar(alphabet);
     }
 
-  putchar('\n');
-  return (0);
+    putchar('\n');
+    return (0);
 }
