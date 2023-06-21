353ed302e65c1ff2a8eeac2869ab870d4e584040
Alalade Ayodeji <ayodejialalade29@gmail.com>
2023-06-21 15:12:21 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@
 */
 
 int main(void)
-  {
+{
      _putchar('_');
      _putchar('p');
      _putchar('u');
@@ -18,7 +18,6 @@ int main(void)
      _putchar('h');
      _putchar('a');
      _putchar('r');
-
      _putchar('\n');
      return (0);
-  }
+}
